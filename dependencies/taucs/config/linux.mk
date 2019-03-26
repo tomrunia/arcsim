9e1be25a39e6908ca6be94ee901b7d4c4a5226aa
Tom Runia <tomrunia@gmail.com>
2019-03-26 15:51:44 +0100

    minor

--- dependencies/taucs/config/linux.mk
+++ dependencies/taucs/config/linux.mk
@@ -18,8 +18,8 @@ CFLAGS    = -g -O3 -Wall -Werror -pedantic -ansi
 # for some reason, -std=c99 -pedantic crashes 
 # with the error message "imaginary constants are a GCC extension"
 # (seems to be a gcc bug, gcc 3.3.1)
-CFLAGS    = -O3 -Wall -Werror -std=c89 -pedantic
-CFLAGS    = -O3 -Wall -Werror -std=c99 
+CFLAGS    = -O3 -Wall -std=c89 -pedantic
+CFLAGS    = -O3 -Wall -std=c99 
 CFLAGS    = -O3 -Wall -std=c99
 
 LD        = $(CC) 
